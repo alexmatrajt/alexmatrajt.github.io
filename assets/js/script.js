4e6151e3ec698f9929917c59e45829b101279911
alexmatrajt <alexmat@umich.edu>
2025-05-13 02:33:50 -0600

    reverted tab changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,11 +83,7 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     for (let i = 0; i < pages.length; i++) {
-      // Convert both strings to lowercase and trim for comparison
-      const navText = this.innerHTML.toLowerCase().trim();
-      const pageData = pages[i].dataset.page.toLowerCase().trim();
-      
-      if (navText === pageData) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
